7f47a30d6a4e8e60b951de4a5a94006c0b423913
Tanner Bebb <17tbebb@gmail.com>
2024-01-24 07:43:59 -0800

    Initial setup

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -5,6 +5,33 @@ do some basic math functions*/
 #include <iostream>
 using namespace std;
 
+void Multiplication();
+
+void Division();
+
+void Addition();
+
+void Subtraction();
+
 int main() {
+	int Choice, x, y;
+
+	cout << "Welcome, what would you like to do?: " << endl;
+
+}
+
+void Multiplication() {
+
+}
+
+void Division() {
+
+}
+
+void Addition() {
+
+}
+
+void Subtraction() {
 
 }
