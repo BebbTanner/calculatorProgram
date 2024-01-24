666c117bb980cf7854133c39e7b0e4d976115ec6
Tanner Bebb <17tbebb@gmail.com>
2024-01-24 08:00:35 -0800

    Added a nested if else state to allow the user to choose what operation that they would like to do.

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -14,9 +14,44 @@ void Addition();
 void Subtraction();
 
 int main() {
-	int Choice, x, y;
+	int x, y;
+	char Choice;
 
 	cout << "Welcome, what would you like to do?: " << endl;
+	cin >> Choice;
+
+	if (Choice == 'A' || Choice == 'a') {
+		Addition();
+	}
+	else if (Choice == 'S' || Choice == 's') {
+		Subtraction();
+	}
+	else if (Choice == 'M' || Choice == 'm') {
+		Multiplication();
+	}
+	else if (Choice == 'D' || Choice == 'd') {
+		Division();
+	}
+	/*My switch case is not working so I am going to try to do 
+	this using a nested if-else statement*/
+	
+	//switch (Choice) {
+	//	case('M' || 'm'):
+	//		Multiplication();
+	//		break;
+	//	case('D' || 'd'):
+	//		Division();
+	//		break;
+
+	//	case('A' || 'a'):
+	//		Addition();
+	//		break;
+
+	//	case('S' || 's'):
+	//		Subtraction();
+	//		break;
+	//}
+
 
 }
 
