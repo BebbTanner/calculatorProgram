fc27c154ff823cd9161bf99fe8b65f51c723ed8b
Tanner Bebb <17tbebb@gmail.com>
2024-01-24 08:15:58 -0800

    Created the definition for the multiplication function underneath int main

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -3,6 +3,7 @@
 This program will use a switch case and a seris of functions to allow the user to 
 do some basic math functions*/
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 void Multiplication();
@@ -18,6 +19,12 @@ int main() {
 	char Choice;
 
 	cout << "Welcome, what would you like to do?: " << endl;
+	cout << "Your choices are: " << endl;
+	cout << "A for addition." << endl;
+	cout << "S for subtraction." << endl;
+	cout << "M for multiplication" << endl;
+	cout << "D for division." << endl;
+	
 	cin >> Choice;
 
 	if (Choice == 'A' || Choice == 'a') {
@@ -32,31 +39,31 @@ int main() {
 	else if (Choice == 'D' || Choice == 'd') {
 		Division();
 	}
-	/*My switch case is not working so I am going to try to do 
-	this using a nested if-else statement*/
-	
-	//switch (Choice) {
-	//	case('M' || 'm'):
-	//		Multiplication();
-	//		break;
-	//	case('D' || 'd'):
-	//		Division();
-	//		break;
-
-	//	case('A' || 'a'):
-	//		Addition();
-	//		break;
-
-	//	case('S' || 's'):
-	//		Subtraction();
-	//		break;
-	//}
-
 
 }
 
+/*The basic structure of this function is complete. I want to add the
+feature that prevents users from entering something that is not a number.
+I forgot how to do that though, so for now I will leave the if statements in.
+I think it should be implemented as a try and catch error, but once again I 
+cannot remember.*/
 void Multiplication() {
+	int x, y;
 
+	cout << "Please enter your x value: " << endl;
+	cin >> x;
+	//if (x !=) {
+
+	//}
+	cout << "Please enter your y value: " << endl;
+	cin >> y;
+	//if(){
+
+	//}
+
+	int mulSolution = x * y;
+
+	cout << "The answer is: " << mulSolution << endl;
 }
 
 void Division() {
