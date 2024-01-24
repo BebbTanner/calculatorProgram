969028bdbc12fda8f0a99df1f47be7c9f6241592
Tanner Bebb <17tbebb@gmail.com>
2024-01-24 07:39:53 -0800

    Add project files.

---
+++ Calculator/main.cpp
@@ -0,0 +1,10 @@
+/*Calculator program
+
+This program will use a switch case and a seris of functions to allow the user to 
+do some basic math functions*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+}
